9401fe665660e81f3db66df75ec29e3604be0bf5
Josh Rose <josh@etchapps.com>
2015-11-27 14:29:23 +0000

    Don't need nodemon

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,20 +51,6 @@ module.exports = function (grunt) {
         }
       }
     },
-    nodemon: {
-      dev: {
-        script: 'app/app.js',
-        options: {
-          watch: 'app/**/*.js',
-          callback: function(nodemon) {
-            // By default the nodemon output is logged
-            nodemon.on('log', function(event) {
-              console.log(event.colour);
-            });
-          }
-        }
-      }
-    },
     watch: {
       options: {
         nospawn: true,
